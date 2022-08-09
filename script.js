37348b94969da6197ea2844a7efa192892509b12
Fabio <fabiowillian.shr@gmail.com>
2022-08-09 18:49:25 -0300

    Finalizando a entrega de Terça

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Fábio"
+let sobrenome = "Willian"
+let anoAtual = 2022
+let anoNascimento = 1996
+let cidade = "Brasília"
+
+let idade = anoAtual - anoNascimento;
+
+//"Eu sou o Jhon Doyle tenho 31 anos e moro em Curitiba"
+
+console.log(`Eu sou o ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${cidade}.`)
